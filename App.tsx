3a1c8dc27fd02093491e730d53809fb67076bffe
Neezar <rforlyffe@gmaicl.com>
2021-04-19 22:57:50 +0800

    add react navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
+import "react-native-gesture-handler";
 import React from "react";
-import { View } from "react-native";
 import styled from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/Home";
+import User from "./src/User";
+import { Button } from "react-native";
+
+const Stack = createStackNavigator();
 
 const Layout = styled.SafeAreaView`
   flex: 1;
@@ -20,10 +27,22 @@ const Text = styled.Text`
 
 export default function App() {
   return (
-    <Layout>
-      <Container>
-        <Text>Please help me</Text>
-      </Container>
-    </Layout>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" mode="modal">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="User"
+          component={User}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <Button
+                title="Back HOMEE"
+                onPress={() => navigation.navigate("Home")}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/User.tsx
@@ -0,0 +1,32 @@
+import { useNavigation, RouteProp, useRoute } from "@react-navigation/core";
+import React, { FC } from "react";
+import { Button } from "react-native";
+import styled from "styled-components/native";
+
+const Layout = styled.View`
+  background-color: teal;
+  flex: 1;
+`;
+
+const Header = styled.Text`
+  font-size: 14px;
+`;
+
+type IRoute = RouteProp<{ user: { id: number; code: string } }, "user">;
+
+const User: FC = () => {
+  const navigation = useNavigation();
+  const route = useRoute<IRoute>();
+  const { code, id } = route.params;
+
+  return (
+    <Layout>
+      <Header>
+        User PAGE {code} {id}
+      </Header>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </Layout>
+  );
+};
+
+export default User;
